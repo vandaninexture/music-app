59eeb00591d6cebee9d59b8239f9b8b8c695e574
vandaninexture <vandan.inexture@gmail.com>
2022-09-28 12:40:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+// import style from "../assets/css/components/audio.module.scss";
+// import playAudioIcon from "../assets/images/home/svg/play_audio.svg";
+// import pauseAudioIcon from "../assets/images/home/svg/pause_audio.svg";
 
-function App() {
+import audioSource from "./bullpreme-theme-song.mp3";
+
+const App = () => {
+  const [isPlaying, setPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  const playAudio = () => {
+    setPlaying(false);
+    audioRef.current.pause();
+  };
+
+  const pauseAudio = () => {
+    setPlaying(true);
+    audioRef.current.play();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <audio ref={audioRef} autoPlay allow="autoplay" controls loop>
+        <source src={audioSource}></source>
+      </audio>
+
+      {isPlaying ? (
+        <button onClick={playAudio}>
+          {/* <img src={playAudioIcon} alt="music-pause-icon" loading="lazy"/> */}
+          play
+        </button>
+      ) : (
+        <button onClick={pauseAudio}>
+          {/* <img src={pauseAudioIcon} alt="music-play-icon" loading="lazy" /> */}
+          pause
+        </button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
